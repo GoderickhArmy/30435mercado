9b511b70213c27323d4a856758ec21a4b57fa154
jessie <jhesy.92@gmail.com>
2022-07-11 19:33:37 -0300

    Crs'

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -3,17 +3,17 @@ let inversion = parseInt(prompt("Cuanto desea invertir?: "));
 
 function invest(inversion) {
 
-    if (inversion < 1000 && isNaN(inversion)) {
+    if (inversion < 1000 || isNaN(inversion)) {
         alert("Error, la inversion debe ser mayor a Mil");
     } else {
-        let intereses = parseInt(prompt("Interes anual?: "));
+        let intereses = parseFloat(prompt("Interes anual?: "));
         if (intereses == 0) {
             alert("Error: El interes no puede ser cero");
         } else {
             let años = parseInt(prompt("Años?: "));
 
-            for (i = 1; i <= años; i++) {
-                inversion *= 1 + intereses / 100;
+            for (let i = 1; i <= años; i++) {
+                inversion += 1 + intereses / 100;
                 alert("Capital tras " + i + " años $" + parseInt(inversion));
             }
         }
