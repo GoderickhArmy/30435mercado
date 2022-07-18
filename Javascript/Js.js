ae0bcb417602fe7a8696628c25ed12fa6f3bda57
jessie <jhesy.92@gmail.com>
2022-07-17 21:37:14 -0300

    cambios

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -1,3 +1,28 @@
+let inversion = parseInt(prompt("Cuanto desea invertir?: "));
+
+function invest(inversion) {
+
+    if (inversion < 1000 || isNaN(inversion)) {
+        alert("Error, la inversion debe ser mayor a Mil");
+    } else {
+        let intereses = parseFloat(prompt("Interes anual?: "));
+        if (intereses == 0) {
+            alert("Error: El interes no puede ser cero");
+        } else {
+            let años = parseInt(prompt("Años?: "));
+
+            for (let i = 1; i <= años; i++) {
+                inversion += 1 + intereses / 100;
+                alert("Capital tras " + i + " años $" + parseInt(inversion));
+            }
+        }
+
+    }
+}
+
+invest(inversion);
+
+
 class Usuario {
     constructor(nombre, apellido, edad, inversion) {
         this.nombre = nombre;
@@ -25,42 +50,20 @@ const crearUsuario = () => {
     return (usuario1);
     usuariosExistentes.push(usuario1);
 
-
-    let verUsuario = prompt("Desea ver el usuario creado?:")
-    if (verUsuario.toLowerCase == "si") {
-        console.log(usuariosExistentes);
-    } if (verUsuario.toLowerCase == "no") {
-        prompt("Hasta luego!");
-    }
-
 }
 
+let decisionUsuario = prompt("Desea crear un usuario?");
 
+if(decisionUsuario.toLowerCase == 'si'){
+    crearUsuario();
 
-
-let inversion = parseInt(prompt("Cuanto desea invertir?: "));
-
-function invest(inversion) {
-
-    if (inversion < 1000 || isNaN(inversion)) {
-        alert("Error, la inversion debe ser mayor a Mil");
-    } else {
-        let intereses = parseFloat(prompt("Interes anual?: "));
-        if (intereses == 0) {
-            alert("Error: El interes no puede ser cero");
-        } else {
-            let años = parseInt(prompt("Años?: "));
-
-            for (let i = 1; i <= años; i++) {
-                inversion += 1 + intereses / 100;
-                alert("Capital tras " + i + " años $" + parseInt(inversion));
-            }
-        }
-
-    }
+let verUsuario = prompt("Desea ver el usuario creado?:")
+if (verUsuario.toLowerCase == 'si') {
+    console.log(usuariosExistentes);
+} if (verUsuario.toLowerCase == "no") {
+    alert("Hasta luego!");
+}else {
+    alert("Que tenga un buen dia");
 }
 
-invest(inversion);
-
-
-
+}
