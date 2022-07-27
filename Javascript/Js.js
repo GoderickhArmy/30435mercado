b2e8510070fef90a460b8af10e3d7a5478b2e8f0
jessie <jhesy.92@gmail.com>
2022-07-27 16:49:53 -0300

    Unnskyld

--- Javascript/Js.js
+++ Javascript/Js.js
@@ -1,9 +1,4 @@
-//Array------------------------------------
-
 let usuariosExistentes = [];
-
-//DATA-------------------------------------
-
 
 let nombre = document.querySelector("#nombre")
 let apellido = document.querySelector("#apellido");
@@ -12,9 +7,11 @@ let inversion = document.querySelector("#inversion");
 let lista = document.createElement("ul");
 let mostrarUsuario = document.querySelector("#usuarios");
 let boton = document.querySelector("#enviar");
+let years = document.querySelector("#years");
 
 
-//Class and constr--Arrowfuntion-------------------------
+//Class-constr-method-----------------------
+
 
 class Usuario {
     constructor(nombre, apellido, edad, inversion) {
@@ -22,14 +19,18 @@ class Usuario {
         this.apellido = apellido;
         this.edad = edad;
         this.inversion = parseFloat(inversion);
+        this.years = parseInt(years);
     }
+calculoGanancia() {
+    let resultadoGanancia = (this.inversion * 100 / 50) * this.years;
+    return resultadoGanancia;
 };
 
+};
 const crearUsuario = () => {
-    const usuario1 = new Usuario(nombre.value, apellido.value, edad.value, inversion.value);
+    const usuario1 = new Usuario(nombre.value, apellido.value, edad.value, inversion.value, years.value);
     usuariosExistentes.push(usuario1);
 };
-
 
 //EVENTO/DOM------------------------------------------------------------------------------
 
@@ -42,30 +43,29 @@ boton.addEventListener("click", elemento => {
         mostrarUsuario.innerHTML = `<p class= "text-danger"> Error, ingrese el monto a invertir!</p>`
     } else {
         crearUsuario()
-        let anos = document.querySelector("#years");
         usuariosExistentes.forEach(elemento => {
-            lista.innerHTML = 
-            `<div class="containerServicios">
-            <figure>
+            lista.innerHTML =
+                `<div class="containerServicios">
+            <div class="figure">
                 <div class="capa">
 
                     <h3 class="h4tituloservicio">Ultima actualizacion</h3>
 
+                    <ul>
                     <li>Nombre: ${elemento.nombre}</li>
                     <li>Apellido: ${elemento.apellido}</li>
                     <li>Edad: ${elemento.edad}</li>
-                    <li>Inversion: ${elemento.inversion}</li></p>
-
+                    <li>Inversion: ${elemento.inversion}</li>
+                    <li>Ganancia:${elemento.calculoGanancia()}</li>
+                    </ul>
                     <img class="imgservicio" src="./img/dinero.jpg" alt="">
                     
 
             </div>
-            </figure>
+            </div>
             </div> `
         });
 
         mostrarUsuario.appendChild(lista);
-
-
     }
 });
